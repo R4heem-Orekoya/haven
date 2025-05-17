86efea581d4d01308c3c9759928b02681feabf69
Raheem Orekoya <tundeorekoya1@gmail.com>
2025-05-17 05:24:22 +0100

    fixed linting issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import authConfig from "./auth.config";
 import NextAuth from "next-auth";
-import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from "@/routes"
+import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes } from "@/routes"
 import { isMatchingPublicRoute } from "./lib/utils";
 
 const { auth: middleware } = NextAuth(authConfig)
