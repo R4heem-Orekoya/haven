a5381124ef7bae56e0b2d7fdb0ded7d7c04d7086
Raheem Orekoya <tundeorekoya1@gmail.com>
2025-05-17 03:57:12 +0100

    property carousel now working

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -2,10 +2,7 @@ import PropertyCarousel from "@/components/property/property-carousel"
 import PropertyDetails from "@/components/property/property-details"
 import { db } from "@/lib/db"
 import { currentUser } from "@/lib/db/queries/user"
-import { cn } from "@/lib/utils"
-import Image from "next/image"
 import { notFound } from "next/navigation"
-import PlaceHolderImage from "../../../../public/placeholder.svg"
 
 interface Props {
    params: Promise<{
@@ -38,37 +35,10 @@ const Page = async ({ params }: Props) => {
 
    return (
       <main className="container mx-auto px-4 md:px-6 lg:px-8 py-6 md:py-12">
-         <div className="grid md:grid-cols-2 gap-4 w-full">
-            <div className={cn("relative col-span-1 bg-zinc-100 rounded-lg border overflow-hidden max-md:aspect-[4/3] cursor-pointer", { "h-[450px]": property.images.length < 3 })}>
-               <Image
-                  src={images[0].status === "processing" ? PlaceHolderImage : images[0].url}
-                  alt={`${property.title} thumbnail`}
-                  fill
-                  className={cn("object-cover", { "animate-pulse": images[0].status === "processing" })}
-                 placeholder={property.images[0].hash ? "blur" : "empty"}
-                  blurDataURL={images[0].hash ?? undefined}
-               />
-            </div>
-            <div className="col-span-1 grid grid-cols-2 gap-4 max-md:hidden">
-               {images.slice(1, 5).map((image, i) => (
-                  <div key={image.id} className="relative aspect-[4/3] rounded-lg overflow-hidden border bg-zinc-100 cursor-pointer">
-                     <Image
-                        src={image.status === "processing" ? PlaceHolderImage : image.url}
-                        alt={`${property.title} image`}
-                        fill
-                        className={cn("object-cover", { "animate-pulse": image.status === "processing" })}
-                        placeholder={property.images[0].hash ? "blur" : "empty"}
-                        blurDataURL={image.hash ?? undefined}
-                     />
-                     {property.images.length > 5 && i === property.images.slice(1, 5).length - 1 && (
-                        <div className="absolute grid place-items-center inset-0 bg-black/60 backdrop-blur-sm">
-                           <span className="text-4xl font-medium text-white">+{property.images.length - 5}</span>
-                        </div>
-                     )}
-                  </div>
-               ))}
-            </div>
-         </div>
+         <PropertyCarousel 
+            images={images} 
+            property={property}
+         />
 
          <PropertyDetails
             property={property}
--- src/components/property/property-carousel.tsx
+++ src/components/property/property-carousel.tsx
@@ -1,80 +1,194 @@
 "use client"
 
-import { Image as TImage } from "@prisma/client"
 import Image from "next/image"
-import { useState } from "react"
-import SavePropertyButton from "./save-property-button"
-import { Loader2, MapPin } from "lucide-react"
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel"
+import { useEffect, useRef, useState } from "react"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog"
+import { PropertyWithFavoritesAndImages } from "@/types/property"
+import { cn } from "@/lib/utils"
+import PlaceHolderImage from "../../../public/placeholder.svg"
+import { Image as TImage } from "@prisma/client"
+import { ArrowLeft, ArrowRight, X } from "lucide-react"
+import { Button } from "../ui/button"
+
+import SwiperCore from "swiper"
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react"
+import { Thumbs } from "swiper/modules"
+import "swiper/css"
+import "swiper/css/thumbs"
 
 interface PropertyCarouselProps {
+   property: PropertyWithFavoritesAndImages;
    images: TImage[]
-   propertyId: string
-   location: string
 }
 
-const PropertyCarousel = ({ images, location, propertyId }: PropertyCarouselProps) => {
-   const [selectedIndex, setSelectedIndex] = useState(0)
+export default function PropertyCarousel({ property, images }: PropertyCarouselProps){
+   const [isDialogOpen, setIsDialogOpen] = useState(false)
+   const [thumbsSwiper, setThumbsSwiper] = useState<SwiperCore | null>(null)
+   const [currentIndex, setCurrentIndex] = useState(0)
+    const swiperRef = useRef<SwiperCore | null>(null)
 
    return (
-      <div className="col-span-1 max-h-fit">
-         <div className="relative rounded-xl overflow-hidden border">
-            <div className="absolute top-4 left-1/2 -translate-x-1/2 z-[60] flex items-start justify-between w-[calc(100%-28px)]">
-               <SavePropertyButton propertyId={propertyId} />
-
-               <div className="px-2 py-1 flex items-center gap-1 bg-secondary text-secondary-foreground text-xs font-semibold rounded-3xl">
-                  <MapPin className="w-3 h-3" />
-                  {location}
-               </div>
+      <>
+         <div className="grid md:grid-cols-2 gap-4 w-full">
+            <div
+               onClick={() =>
+                  setIsDialogOpen(true)
+               }
+               className={
+                  cn("relative col-span-1 bg-zinc-100 rounded-lg border overflow-hidden max-md:aspect-[4/3] cursor-pointer",
+                     { "h-[450px]": property.images.length < 3 })
+               }>
+               <Image
+                  src={images[0].status === "processing" ? PlaceHolderImage : images[0].url}
+                  alt={`${property.title} thumbnail`}
+                  fill
+                  className={cn("object-cover", { "animate-pulse": images[0].status === "processing" })}
+                  placeholder={property.images[0].hash ? "blur" : "empty"}
+                  blurDataURL={images[0].hash ?? undefined}
+               />
             </div>
-            <div className="relative aspect-square bg-neutral-200">
-               {images[selectedIndex].status === "failed" && (
-                  <p>Image Upload Failed</p>
-               )}
-               {images[selectedIndex].status === "processing" && (
-                  <Loader2 className="w-8 h-8 animate-spin" />
-               )}
-               {images[selectedIndex].status === "uploaded" && images[selectedIndex].url && (
-                  <Image
-                     src={images[selectedIndex].url}
-                     alt="Property Image thumbnail" fill
-                     className="object-cover"
-                  />
-               )}
+            <div className="col-span-1 grid grid-cols-2 gap-4 max-md:hidden">
+               {images.slice(1, 5).map((image, i) => (
+                  <div
+                     onClick={() =>
+                        setIsDialogOpen(true)
+                     }
+                     key={image.id}
+                     className="relative aspect-[4/3] rounded-lg overflow-hidden border bg-zinc-100 cursor-pointer"
+                  >
+                     <Image
+                        src={image.status === "processing" ? PlaceHolderImage : image.url}
+                        alt={`${property.title} image`}
+                        fill
+                        className={cn("object-cover", { "animate-pulse": image.status === "processing" })}
+                        placeholder={property.images[0].hash ? "blur" : "empty"}
+                        blurDataURL={image.hash ?? undefined}
+                     />
+                     {property.images.length > 5 && i === property.images.slice(1, 5).length - 1 && (
+                        <div className="absolute grid place-items-center inset-0 bg-black/60 backdrop-blur-sm">
+                           <span className="text-4xl font-medium text-white">+{property.images.length - 5}</span>
+                        </div>
+                     )}
+                  </div>
+               ))}
             </div>
          </div>
 
-         <Carousel className="mt-4 sm:mt-6 w-full">
-            <CarouselContent>
-               {images.map((image, index) => (
-                  <CarouselItem
-                     key={image.id}
-                     onClick={() => setSelectedIndex(index)}
-                     className="basis-1/3"
+         <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <DialogContent
+               showCloseButton={false}
+               className="max-w-screen h-screen sm:rounded-none border-none bg-black/80 flex flex-col justify-between"
+            >
+               <DialogHeader className="flex flex-row items-center justify-between w-full text-white">
+                  <DialogTitle>
+                     {currentIndex + 1} / {images.length}
+                  </DialogTitle>
+
+                  <Button 
+                     onClick={() => {
+                        setThumbsSwiper(null)
+                        setIsDialogOpen(false)
+                     }}
+                     size="icon" 
+                     variant="ghost"
                   >
-                     <div className="relative aspect-square cursor-pointer">
-                        {image.status === "failed" && (
-                           <p>Image Upload Failed</p>
-                        )}
-                        {image.status === "processing" && (
-                           <Loader2 className="w-8 h-8 animate-spin" />
-                        )}
-                        {image.status === "uploaded" && image.url && (
-                           <Image 
-                              src={image.url} 
-                              alt="image" fill 
-                              className="object-cover w-full h-full rounded-lg" 
+                     <X />
+                  </Button>
+               </DialogHeader>
+               
+               {isDialogOpen && (
+                  <Swiper
+                     modules={[Thumbs]}
+                     onSlideChange={(swiper) => setCurrentIndex(swiper.activeIndex)}
+                     navigation
+                     thumbs={{ swiper: thumbsSwiper }}
+                     onSwiper={(swiper) => (swiperRef.current = swiper)}
+                     className="w-full h-[80%] rounded-lg overflow-hidden"
+                  >
+                     {images.map((img) => (
+                        <SwiperSlide key={img.id}>
+                           <img
+                              src={img.url ?? PlaceHolderImage}
+                              alt={img.url as string}
+                              className="w-full h-full object-contain"
                            />
-                        )}
-                     </div>
-                  </CarouselItem>
-               ))}
-            </CarouselContent>
-            <CarouselPrevious className="left-0 z-[90]" />
-            <CarouselNext className="right-0 z-[90]" />
-         </Carousel>
-      </div>
+                        </SwiperSlide>
+                     ))}
+                     <SliderControls />
+                  </Swiper>
+               )}
+
+               {/* Swiper Thumbnails */}
+               {isDialogOpen && (
+                  <div className="mt-4">
+                     <Swiper
+                        modules={[Thumbs]}
+                        onSwiper={setThumbsSwiper}
+                        slidesPerView={3}
+                        breakpoints={{
+                           640: { slidesPerView: 3 },
+                           768: { slidesPerView: 5 },
+                           1024: { slidesPerView: 8 },
+                        }}
+                        spaceBetween={10}
+                        watchSlidesProgress
+                        className="w-full"
+                     >
+                        {images.map((img) => (
+                           <SwiperSlide key={img.id} className="cursor-pointer">
+                              <img
+                                 src={img.url ?? PlaceHolderImage}
+                                 alt=""
+                                 className="w-full h-20 object-cover rounded border border-zinc-700"
+                              />
+                           </SwiperSlide>
+                        ))}
+                     </Swiper>
+                  </div>
+               )}
+            </DialogContent>
+         </Dialog>
+      </>
    )
 }
 
-export default PropertyCarousel
+function SliderControls() {
+   const swiper = useSwiper()
+   const [isBeginning, setIsBeginning] = useState(true)
+   const [isEnd, setIsEnd] = useState(false)
+
+   useEffect(() => {
+      const update = () => {
+         setIsBeginning(swiper.isBeginning)
+         setIsEnd(swiper.isEnd)
+      }
+
+      update()
+      swiper.on("slideChange", update)
+
+      return () => {
+         swiper.off("slideChange", update)
+      }
+   }, [swiper])
+
+   return (
+      <div className="absolute top-1/2 -translate-y-1/2 z-20 w-full flex justify-between px-4">
+         <Button
+            onClick={() => swiper.slidePrev()}
+            size="icon" variant="ghost"
+            className="rounded-full"
+            disabled={isBeginning}
+         >
+            <ArrowLeft className="w-5 h-5 text-muted-foreground" />
+         </Button>
+         <Button
+            onClick={() => swiper.slideNext()}
+            size="icon" variant="ghost"
+            className="rounded-full"
+            disabled={isEnd}
+         >
+            <ArrowRight className="w-5 h-5 text-muted-foreground" />
+         </Button>
+      </div>
+   )
+}
