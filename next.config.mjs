ef1a90a465333d0f14a74c902df5912a2ba1fab7
Raheem Orekoya <tundeorekoya1@gmail.com>
2025-05-20 15:49:00 +0100

    added uploadthing to next-config and updated metadata

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   images: {
-      remotePatterns: [
-         {
-         protocol: "https",
-         hostname: "images.unsplash.com",
-         port: "",
-         },
-         {
-         protocol: "https",
-         hostname: "utfs.io",
-         port: "",
-         },
-         {
-         protocol: "https",
-         hostname: "pgokm0g6hg.ufs.sh",
-         pathname: "/f/*",
-         port: "",
-         },
-      ],
-   },
-
-      experimental: {
-         authInterrupts: true,
-      serverActions: {
-         bodySizeLimit: "8mb",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
       },
-   },
+      {
+        protocol: "https",
+        hostname: "pgokm0g6hg.ufs.sh",
+        pathname: "/f/*",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "n46wwp1z8y.ufs.sh",
+        pathname: "/f/*",
+        port: "",
+      },
+    ],
+  },
+
+  experimental: {
+    authInterrupts: true,
+    serverActions: {
+      bodySizeLimit: "8mb",
+    },
+  },
 };
 
 export default nextConfig;
